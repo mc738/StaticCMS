a4c6f48170198246e65ba0c28fe6dc85d2dd0904
Max Clifford <me@mclifford.dev>
2024-01-05 18:14:38 +0000

    Added InteractiveMode.fs

---
+++ StaticCMS.App/InteractiveMode.fs
@@ -0,0 +1,19 @@
+namespace StaticCMS.App
+
+open StaticCMS.App.Actions
+
+
+
+module InteractiveMode =
+    
+    
+    let handleAction (ctx: AppContext) (site: string) =
+        
+        ()
+    
+    
+    let run _ =
+        
+        
+        ()
+
