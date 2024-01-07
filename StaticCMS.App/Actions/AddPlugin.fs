a522ad556435ddd2f5f9f5fde344b7b30b90bc86
Max Clifford <me@mclifford.dev>
2024-01-07 18:17:00 +0000

    Added AddPlugin.fs

---
+++ StaticCMS.App/Actions/AddPlugin.fs
@@ -0,0 +1,12 @@
+namespace StaticCMS.App.Actions
+
+open StaticCMS.App.Common
+
+module AddPlugin =
+
+    let action (ctx: AppContext) (name: string) (pluginType: string) =
+        match ctx.Store.GetPlugin name with
+        | Some p -> ActionResult.Skipped($"Plugin `{name}` already exists.", Some p.Name)
+        | None ->
+            ctx.Store.AddPlugin(name, pluginType)
+            ActionResult.Success($"Plugin `{name}` added.", None)
