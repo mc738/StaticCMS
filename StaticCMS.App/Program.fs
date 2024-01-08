f5e33a83eee8a9aada34728d18b152dc6f2182ce
Max Clifford <me@mclifford.dev>
2024-01-08 18:24:29 +0000

    Fixed up Program.fs

--- StaticCMS.App/Program.fs
+++ StaticCMS.App/Program.fs
@@ -1,49 +1,36 @@
 ﻿open System
-open System.IO
-open System.Text
-open System.Text.Json
-open System.Text.Json.Serialization
-open Fluff.Core
 open FsToolbox.AppEnvironment.Args
-open FsToolbox.AppEnvironment.Args.Mapping
 open FsToolbox.Core
-open StaticCMS
-open StaticCMS.DataStore
-open StaticCMS.Pipeline
-open Wikd.DataStore
+open StaticCMS.App.Actions
 open StaticCMS.App
+open StaticCMS.App.Common.Options
 
-let options =
-    Environment.GetCommandLineArgs()
-    |> List.ofArray
-    |> ArgParser.tryGetOptions<Options>
+let args = Environment.GetCommandLineArgs()
 
-let getGeneralSettings (path: string option) =
-    path
-    |> Option.defaultWith (fun _ -> Path.Combine(getStaticRoot (), "settings.json"))
-    |> fun p ->
-        try
-            File.ReadAllText p |> JsonSerializer.Deserialize<GeneralSettings> |> Ok
-        with exn ->
-            Error "Failed to deserialize general settings"
+match args.Length > 1 with
+| true ->
+    let options =
+        Environment.GetCommandLineArgs()
+        |> List.ofArray
+        |> ArgParser.tryGetOptions<Options>
 
-let result =
-    options
-    |> Result.bind (fun o ->
-        match o with
-        | InitializeSite initializeSiteOptions -> initializeSite initializeSiteOptions
-        | AddSite addSiteOptions -> failwith "todo"
-        | ImportTemplate importTemplateOptions -> importTemplate importTemplateOptions
-        | RenderSite renderSiteOptions -> renderSite renderSiteOptions
-        | AddPath addPageOptions -> failwith "todo"
-        | ImportFragmentTemplate importFragmentTemplateOptions -> importFragmentTemplate importFragmentTemplateOptions
-        | AddSitePlugin -> failwith "todo"
-        | RunInit -> failwith "todo")
+    let result =
+        options
+        |> Result.bind (fun o ->
+            match o with
+            | InitializeSite initializeSiteOptions -> InitializeSite.action initializeSiteOptions
+            | AddSite addSiteOptions -> failwith "todo"
+            | ImportTemplate importTemplateOptions -> ImportTemplate.action importTemplateOptions
+            | RenderSite renderSiteOptions -> RenderSite.action renderSiteOptions
+            | AddPath addPageOptions -> failwith "todo"
+            | ImportFragmentTemplate importFragmentTemplateOptions -> ImportFragmentTemplate.action importFragmentTemplateOptions
+            | AddSitePlugin -> failwith "todo"
+            | RunInit -> failwith "todo")
 
-match result with
-| Ok _ -> ()
-| Error e -> ConsoleIO.printError e
-
+    match result with
+    | Ok _ -> ()
+    | Error e -> ConsoleIO.printError e
+| false -> InteractiveModeV2.run ()
 
 // For more information see https://aka.ms/fsharp-console-apps
 //printfn "Hello from F#"
