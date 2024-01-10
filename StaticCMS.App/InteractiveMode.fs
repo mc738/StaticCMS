5772e84e0f39704aa6bfa890ed9822296868fe3a
Max Clifford <me@mclifford.dev>
2024-01-10 20:01:58 +0000

    Updated InteractiveMode.fs

--- StaticCMS.App/InteractiveMode.fs
+++ StaticCMS.App/InteractiveMode.fs
@@ -1,137 +1,210 @@
 namespace StaticCMS.App
 
 open System
-open FsToolbox.Core
+open System.ComponentModel.DataAnnotations
+open Spectre.Console
+open StaticCMS.Actions.Common
+open StaticCMS.App.Common
 
 module InteractiveMode =
 
-    open StaticCMS.App.Actions
+    open FsToolbox.Extensions.Strings
+    open StaticCMS.DataStore
+    open StaticCMS.App.Common.Logging
+    //open StaticCMS.App.Actions
     open StaticCMS.App.Common
+    open StaticCMS.Actions.Core
 
-    [<AutoOpen>]
-    module Helpers =
-
-        type UserOption =
-            { Key: string
-              InputValue: string
-              Message: string
-              Hidden: bool
-              Normalize: bool }
-
-            static member Create(key, inputValue, message, ?hidden, ?normalize) =
-                { Key = key
-                  InputValue = inputValue
-                  Message = message
-                  Hidden = hidden |> Option.defaultValue false
-                  Normalize = normalize |> Option.defaultValue false }
-
-        let stringToOption (str: string) =
-            match String.IsNullOrWhiteSpace str with
-            | true -> None
-            | false -> Some str
-
-        let readInput _ = Console.ReadLine()
-
-        let readOptionalInput _ = readInput () |> stringToOption
-
-        let inputPrompt (message: string) =
-            Console.WriteLine message
-            printf "> "
-
-            readInput ()
-
-        let optionalInputPrompt (message: string) =
-            Console.WriteLine message
-            printf "> "
-
-            readOptionalInput ()
-
-        let optionPrompt (message: string) (options: UserOption list) =
-            let rec handle () =
-                printf "> "
-
-                let input = Console.ReadLine()
-
-                match options |> List.tryFind (fun o -> o.InputValue = input) with
-                | Some o -> o
-                | None ->
-                    ConsoleIO.printWarning $"Invalid option `{input}`"
-                    handle ()
-
-            Console.WriteLine message
-
-            options
-            |> List.filter (fun o -> o.Hidden |> not)
-            |> List.iter (fun o -> Console.WriteLine o.Message)
-
-            handle ()
-
-    let handleAction (ctx: AppContext) (site: string) =
-
-        ()
-
-    let createOrLoadSite (ctx: AppContext) =
-
-        printfn "Please select an option:"
-        printfn "1. Create a new site"
-        printfn "2. Load a site"
-
-        let selectedOption = readInput
-
-        let createSite () =
-
-            let siteName = inputPrompt "Enter new site name:"
-            let url = inputPrompt "Enter new site url:"
-
-            let root =
-                inputPrompt $"Enter site root directory (if blank or empty default path will be used):"
-
-
-            failwith "TODO"
+    let textPrompt prompt (optional: bool) =
+        let tp = TextPrompt<string>(prompt)
+        
+        tp.PromptStyle <- "green"
+        if optional then
+            tp.AllowEmpty <- true
             
-            (*
-            match InitializeSite.run ctx siteName url None with
-            | Ok _ -> Ok siteName
-            | Error e -> Error e
-*)            
+        AnsiConsole.Prompt(tp) 
+    
+    
+    let createContext (storePath: string option) (generalSettingsPath: string option) (logger: LogItem -> unit) =
 
-        let rec loadSite () =
+        getGeneralSettings generalSettingsPath
+        |> Result.map (fun gs ->
 
-            printfn "Enter site name: "
+            { Store = StaticStore.Create <| getStorePath storePath
+              Logger = logger
+              GeneralSettings = gs })
 
+    let createSite (ctx: StaticCMSContext) =
 
-            let siteName = Console.ReadLine()
+        let namePrompt =
+            TextPrompt<string>("Enter new site name: ")
+                .Validate(fun name ->
+                    match System.String.IsNullOrWhiteSpace name with
+                    | true -> ValidationResult.Error "Site name can not be blank"
+                    | false -> ValidationResult.Success())
 
+        namePrompt.PromptStyle <- "green"
+
+        let siteName = AnsiConsole.Prompt(namePrompt)
+
+        let urlPrompt =
+            TextPrompt<string>("Enter new site url: ")
+                .Validate(fun name ->
+                    match System.String.IsNullOrWhiteSpace name with
+                    | true -> ValidationResult.Error "Url name can not be blank"
+                    | false -> ValidationResult.Success())
+
+        urlPrompt.PromptStyle <- "green"
+
+        let url = AnsiConsole.Prompt(urlPrompt)
+
+        let displayNamePrompt =
+            TextPrompt<string>("Enter new site display name (if left blank default site name will be used): ")
+
+        displayNamePrompt.PromptStyle <- "green"
+
+        displayNamePrompt.AllowEmpty <- true
+
+        let displayName = AnsiConsole.Prompt(displayNamePrompt)
+
+        let rootPrompt =
+            TextPrompt<string>("Enter new site root path (if left blank default path will be used): ")
+
+        rootPrompt.PromptStyle <- "green"
+
+        rootPrompt.AllowEmpty <- true
+
+        let root = AnsiConsole.Prompt(rootPrompt)
+
+        ({ Name = siteName
+           Url = url
+           DisplayName = displayName.ToOption()
+           Root = root.ToOption() }
+        : InitializeSite.Parameters)
+        |> InitializeSite.run ctx
+        |> Result.bind (fun _ ->
             match ctx.Store.GetSite siteName with
-            | Some _ -> Ok siteName
-            | None ->
-                ConsoleIO.printWarning $"Site `{siteName}` not found"
-                loadSite ()
+            | Some site -> Ok site
+            | None -> Error "Site not created")
 
+    let loadSite (ctx: StaticCMSContext) =
 
-        let option =
-            [
-                UserOption.Create("new-site", "1", "1. Create a new site")
-                UserOption.Create("load-site", "2", "2. Load a site")
-            ]
-            |> optionPrompt "Please select an option:"
-        
-        match option.Key with
-        | "new-site" -> createSite ()
-        | "load-site" -> loadSite ()
-        | _ -> Error "Unknown option"
-        
+        let sitesSelectionPrompt =
+            SelectionPrompt<string>()
+                .AddChoices(ctx.Store.ListSites() |> List.map (fun s -> s.Name))
+
+        sitesSelectionPrompt.Title <- "Select site"
+        sitesSelectionPrompt.PageSize <- 10
+
+        let siteName = AnsiConsole.Prompt(sitesSelectionPrompt)
+
+        match ctx.Store.GetSite siteName with
+        | Some site -> Ok site
+        | None -> Error $"Site `{siteName}` not found"
+
     let run _ =
-        
-        
-        match createContext None None with
+        match createContext None None consoleLogger with
         | Ok ctx ->
-            match createOrLoadSite ctx with
-            | Ok siteName->
-                
-                
-                ()
-            | Error e -> ConsoleIO.printError e
-        | Error e -> ConsoleIO.printError $"Failed to create app context. Error: {e}"
-        
-        
+            let createOrLoadSelection =
+                SelectionPrompt<string>()
+                    .AddChoices([| "Create new site"; "Load existing site" |])
+
+            createOrLoadSelection.Title <- "Please select an option"
+
+            let site =
+                match AnsiConsole.Prompt createOrLoadSelection with
+                | "Create new site" -> createSite ctx
+                | "Load existing site" -> loadSite ctx
+                | _ -> Error "Unknown selection"
+
+            match site with
+            | Ok s ->
+                let rec handle _ =
+                    AnsiConsole.Clear()
+
+                    let optionsPrompt =
+                        SelectionPrompt<string>()
+                            .AddChoices([| "Add page"; "Render site"; "[red]Delete site[/]" |])
+
+                    let selection = AnsiConsole.Prompt optionsPrompt
+
+                    match selection with
+                    | "Add page" ->
+                        let pageName = textPrompt "Enter new page name: " false
+                        let pageNameSlug = textPrompt "Enter new page name slug: " false
+                        let pageTitle = textPrompt "Enter new page title: " false
+                        
+                        let includeDefaultStyles = AnsiConsole.Ask<bool>("Include default styles?")
+                        let includeDefaultScripts = AnsiConsole.Ask<bool>("Include default scripts?")
+                        let navigableTo = AnsiConsole.Ask<bool>("Page is navigable to?") 
+                        
+                        
+                        let status = AnsiConsole.Status()
+
+                        //status.AutoRefresh <- false
+                        status.Spinner <- Spinner.Known.Star
+
+                        status.Start(
+                            "Adding page",
+                            fun c ->
+
+                                ({ SiteName = s.Name
+                                   SiteDisplayName = s.DisplayName |> Option.defaultValue s.Name
+                                   SiteRoot = s.RootPath
+                                   PageName = pageName
+                                   PageNameSlug = pageNameSlug
+                                   PageTitle = pageTitle
+                                   PageTemplate = ""
+                                   IncludeDefaultStyles = includeDefaultStyles
+                                   IncludeDefaultScripts = includeDefaultScripts
+                                   NavigableTo = navigableTo
+                                   Styles = []
+                                   Scripts = []
+                                   AdditionalData = Map.empty
+                                   PageFiles = [] }
+                                : AddPage.Parameters)
+                                |> AddPage.run ctx
+
+                                AnsiConsole.MarkupLine("Adding to store")
+
+                                Async.Sleep 1000 |> Async.RunSynchronously
+
+                                AnsiConsole.MarkupLine("Generating page data")
+                                Async.Sleep 1000 |> Async.RunSynchronously
+                        )
+
+                        AnsiConsole.Markup("[green]Complete. Press any key to continue[/]")
+                        Console.ReadLine() |> ignore
+
+                        handle ()
+                    | "Render site" ->
+
+                        match RenderSite.run ctx { SiteName = s.Name; ScriptHost = None } with
+                        | Ok resultValue ->
+
+                            handle ()
+                        | Error errorValue -> ()
+                    | "[red]Delete site[/]" ->
+                        let confirmationPrompt =
+                            TextPrompt<string>(
+                                $"[red]Are you sure you want to delete `{s.Name}`?[/] To confirm enter the site name here: "
+                            )
+
+                        match AnsiConsole.Prompt(confirmationPrompt) with
+                        | v when v.Equals(s.Name) ->
+                            match DeleteSite.run ctx { SiteName = s.Name } with
+                            | Ok _ ->
+                                AnsiConsole.MarkupLine("[red]Site deleted. Press any key to continue[/]")
+                                Console.ReadLine() |> ignore
+                            | Error e -> AnsiConsole.MarkupLineInterpolated($"[red]Error deleting site: {e}[/]")
+
+                        | _ ->
+                            AnsiConsole.MarkupLine("Names do not match. Press any key to return to menu.")
+                            Console.ReadLine() |> ignore
+                            handle ()
+                    | _ -> failwith "Unknown selection"
+
+
+                handle ()
+            | Error e -> AnsiConsole.MarkupInterpolated($"[red]Failed to create or load site. Error: {e}[/]")
+        | Error e -> AnsiConsole.MarkupInterpolated($"[red]Failed to create app context. Error: {e}[/]")
