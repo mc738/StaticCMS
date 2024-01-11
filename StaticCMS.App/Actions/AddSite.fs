4e4af06dddc53e1a33e03b4c1223704076983ab5
Max Clifford <me@mclifford.dev>
2024-01-11 21:17:42 +0000

    Commented out AddSite.fs content

--- StaticCMS.App/Actions/AddSite.fs
+++ StaticCMS.App/Actions/AddSite.fs
@@ -5,8 +5,11 @@ module AddSite =
     open StaticCMS.App.Common
     open StaticCMS.DataStore
 
+    let notInUse = ()
+(*
     let action (ctx: AppContext) (name: string) (url: string) (root: string) =
         match ctx.Store.TryAddSite(name, url, root) with
         | AddSiteResult.Success -> ctx.Log.LogSuccess("add-site", $"Site `{name}` added.")
         | AddSiteResult.AlreadyExists -> ctx.Log.LogInfo("add-site", $"Site `{name}` already exists.")
         | AddSiteResult.Failure e -> ctx.Log.LogError("add-site", $"Failed to add site `{name}`. Error: {e.Message}")
+    *)
