38ade70e22bafc722a8e709799ded8f2a9446a3a
Max Clifford <me@mclifford.dev>
2024-01-11 21:17:21 +0000

    Commented out AddPage.fs content

--- StaticCMS.App/Actions/AddPage.fs
+++ StaticCMS.App/Actions/AddPage.fs
@@ -3,7 +3,10 @@ namespace StaticCMS.App.Actions
 module AddPage =
 
     open StaticCMS.App.Common
-        
+      
+    let notInUse = ()
+    
+    (*
     let addPage (ctx: AppContext) (site: string) (name: string) (nameSlug: string) =
         match ctx.Store.GetPage(site, name) with
         | Some p -> ActionResult.Skipped($"Page `{name}` (site: `{site}`) already exists.", Some p.Reference)
@@ -11,5 +14,5 @@ module AddPage =
             let ref = createRef ()
             ctx.Store.AddPage(ref, site, name, nameSlug)
             ActionResult.Success($"Page `{name}` added to site `{site}`.", Some ref)
-
+    *)
 
