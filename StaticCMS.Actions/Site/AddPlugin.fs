0a959ce2c39899cb242e41ef21a3a2e7bfbde2ec
Max Clifford <me@mclifford.dev>
2024-01-12 21:21:31 +0000

    Added AddPlugin.fs

---
+++ StaticCMS.Actions/Site/AddPlugin.fs
@@ -0,0 +1,25 @@
+namespace StaticCMS.Actions.Site
+
+[<RequireQualifiedAccess>]
+module AddPlugin =
+
+    open System.IO
+    open StaticCMS
+    open StaticCMS.Actions.Common
+    
+    type Parameters =
+        {
+            SiteName: string
+            SiteRoot: string
+            PluginName: string
+        }
+    
+    let run (ctx: StaticCMSContext) (parameters: Parameters) =
+        
+        let pluginCfg = Path.Combine(ctx.RootPath, "plugins", parameters.PluginName, "settings.json")
+        
+        Plugins.Settings.Load pluginCfg
+        |> Result.bind (fun cfg ->
+            Plugins.initializePlugin cfg.Initialization ctx.RootPath parameters.SiteName)
+        
+    
