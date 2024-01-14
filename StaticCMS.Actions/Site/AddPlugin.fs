12cab3f492e6f94acccf273d33d2090c1124f140
Max Clifford <me@mclifford.dev>
2024-01-14 21:29:46 +0000

    Work on AddPlugin.fs

--- StaticCMS.Actions/Site/AddPlugin.fs
+++ StaticCMS.Actions/Site/AddPlugin.fs
@@ -1,25 +1,27 @@
 namespace StaticCMS.Actions.Site
 
+open StaticCMS.Persistence
+
 [<RequireQualifiedAccess>]
 module AddPlugin =
 
     open System.IO
     open StaticCMS
     open StaticCMS.Actions.Common
-    
+
     type Parameters =
-        {
-            SiteName: string
-            SiteRoot: string
-            PluginName: string
-        }
-    
+        { Site: Records.Site
+          PluginName: string
+          Args: (string * string) list
+          PluginSettings: Plugins.Settings }
+
     let run (ctx: StaticCMSContext) (parameters: Parameters) =
-        
-        let pluginCfg = Path.Combine(ctx.RootPath, "plugins", parameters.PluginName, "settings.json")
-        
-        Plugins.Settings.Load pluginCfg
-        |> Result.bind (fun cfg ->
-            Plugins.initializePlugin cfg.Initialization.Steps ctx.RootPath parameters.SiteRoot)
-        
-    
+
+        ({ Steps = parameters.PluginSettings.Initialization.Steps
+           Args = parameters.Args
+           SiteName = parameters.Site.Name
+           SiteDisplayName = parameters.Site.DisplayName |> Option.defaultValue parameters.Site.Name
+           StaticCMSRoot = ctx.RootPath
+           SiteRoot = parameters.Site.RootPath }
+        : Plugins.InitializePluginParameters)
+        |> Plugins.initializePlugin
